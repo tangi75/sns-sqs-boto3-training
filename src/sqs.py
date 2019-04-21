ad23701a8adf035183f0d48787e3e8dadda4df92
Tangi Vass <tangi.vass@schibsted.com>
2019-04-21 15:16:56 +0200

    Create a Dead-Letter Queue and Bind it to a Queue

--- src/sqs.py
+++ src/sqs.py
@@ -1,9 +1,13 @@
 import boto3
+import json
 
 
 QUEUE_NAME = 'MyTest-SQS-Queue'
 QUEUE_NAME_URL = 'https://eu-west-3.queue.amazonaws.com/969616207414/MyTest-SQS-Queue'
 FIFO_QUEUE_NAME = 'MyTestQueue.fifo'
+FIFO_QUEUE_URL = 'https://eu-west-3.queue.amazonaws.com/724579132719/MyTestQueue.fifo'
+QUEUE_FOR_DEAD_LETTER = 'Dead-Letter-Queue-for-Main'
+DEAD_LETTER_MAIN_QUEUE = 'Main-Queue'
 
 
 def sqs_client():
@@ -25,6 +29,30 @@ def create_fifo_queue():
         }
     )
 
+def create_queue_for_dead_letter():
+    return sqs_client().create_queue(
+        QueueName=QUEUE_FOR_DEAD_LETTER
+    )
+
+def create_dead_letter_queue():
+    redrive_policy = {
+        "deadLetterTargetArn": "arn:aws:sqs:eu-west-3:724579132719:Dead-Letter-Queue-for-Main",
+        "maxReceiveCount": 3
+    }
+    return sqs_client().create_queue(
+        QueueName=DEAD_LETTER_MAIN_QUEUE,
+        Attributes={
+            "DelaySeconds": "0",
+            "MaximumMessageSize": "262144",
+            "VisibilityTimeout": "30",
+            "MessageRetentionPeriod": "345600",
+            "ReceiveMessageWaitTimeSeconds": "0",
+            "RedrivePolicy": json.dumps(redrive_policy)
+        }
+    )
+
 if __name__ == '__main__':
     #print(create_sqs_queue())
-    print(create_fifo_queue())
+    # print(create_fifo_queue())
+    # print(create_queue_for_dead_letter())
+    print(create_dead_letter_queue())
